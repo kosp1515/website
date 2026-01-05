235e93f1bd200aeba130d5c0932744268fb540fd
kosp ‎ <kospanosnikos@gmail.com>
2026-01-05 19:29:15 +0200

    final

--- errorEmpty.js
+++ errorEmpty.js
@@ -1,15 +1,16 @@
 function errorEmpty(text,msg){
     const input = document.getElementById(text);
-    const error = document.getElementById(msg);
+    const errorMsg = document.getElementById(msg);
 
-    if(input.value === ''){
+    if(input.value.trim() === ''){
         input.style.borderColor = 'red';
-        input.style.display = 'solid';
-        error.style.display = 'block';
-        error.innerText = 'Παρακαλώ συμπληρώστε όλα τα πεδία.'
+        input.style.borderStyle = 'solid';
+        errorMsg.style.display = 'block';
+        errorMsg.innerText = 'Παρακαλώ συμπληρώστε όλα τα πεδία.'
     } else {
         input.style.borderColor = '#c8cea6';
-        error.style.display = 'none';
+        input.style.borderStyle = 'none';
+        errorMsg.style.display = 'none';
         
     }
     
